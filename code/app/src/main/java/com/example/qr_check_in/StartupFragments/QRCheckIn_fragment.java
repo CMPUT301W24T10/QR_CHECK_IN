1b707e27f7a544330d768fce04d184acc7bdc304
mfafzal <mfafzal@ualberta.ca>
2024-03-07 02:36:33 -0700

    removed unused imports

--- code/app/src/main/java/com/example/qr_check_in/StartupFragments/QRCheckIn_fragment.java
+++ code/app/src/main/java/com/example/qr_check_in/StartupFragments/QRCheckIn_fragment.java
@@ -1,5 +1,5 @@
 package com.example.qr_check_in.StartupFragments;
-import android.content.DialogInterface;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
