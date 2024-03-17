b44925dc1af4bf3f324b842b9888756fb6eca53e
mfafzal <mfafzal@ualberta.ca>
2024-03-17 14:27:25 -0600

    added new dialog box prompting user for info. doesnt look nice

--- code/app/src/main/java/com/example/qr_check_in/StartupFragments/QRCheckIn_fragment.java
+++ code/app/src/main/java/com/example/qr_check_in/StartupFragments/QRCheckIn_fragment.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.result.ActivityResultLauncher;
@@ -97,26 +99,33 @@ public class QRCheckIn_fragment extends Fragment {
                                 QuerySnapshot querySnapshot = task.getResult();
                                 for(DocumentSnapshot documentSnapshot : querySnapshot.getDocuments()){
                                     if(documentSnapshot.getId().equals(uniqueId)){
-                                        Map<String, Object> docData;
-                                        eventTitle = documentSnapshot.getString("eventName");
-                                        eventDescription = documentSnapshot.getString("eventDescription");
-                                        found = true;
-                                        AlertDialog.Builder builder = new AlertDialog.Builder(thisContext);
-                                        builder.setTitle(eventTitle);
-                                        builder.setMessage(eventDescription);
+                                        if(documentSnapshot.contains("attendees")){
+                                            Map<String, String> existingAttendees = (Map<String, String>) documentSnapshot.get("attendees");
+                                            if (existingAttendees.containsKey(deviceId)) {
+                                                Navigation.findNavController(requireView()).navigate(R.id.action_QRCheckIn_fragment_to_attendeeSelection_fragment);
+                                            }else{
+                                                eventTitle = documentSnapshot.getString("eventName");
+                                                eventDescription = documentSnapshot.getString("eventDescription");
+                                                found = true;
+                                                AlertDialog.Builder builder = new AlertDialog.Builder(thisContext);
+                                                builder.setTitle(eventTitle);
+                                                builder.setMessage(eventDescription);
 
-                                        builder.setPositiveButton("Check In", (DialogInterface.OnClickListener) (dialog, which) -> {
-                                            // When the user click yes button then app will close
+                                                builder.setPositiveButton("Check In", (DialogInterface.OnClickListener) (dialog, which) -> {
+                                                   showCustomDialog();
 
-                                            Navigation.findNavController(requireView()).navigate(R.id.action_QRCheckIn_fragment_to_attendeeSelection_fragment);
-                                        });
+                                                });
 
-                                        builder.setNegativeButton("Back", (DialogInterface.OnClickListener) (dialog, which) -> {
-                                            // If user click no then dialog box is canceled.
-                                            dialog.cancel();
-                                        });
-                                        AlertDialog alertDialog = builder.create();
-                                        alertDialog.show();
+                                                builder.setNegativeButton("Back", (DialogInterface.OnClickListener) (dialog, which) -> {
+                                                    // If user click no then dialog box is canceled.
+                                                    dialog.cancel();
+                                                });
+                                                AlertDialog alertDialog = builder.create();
+                                                alertDialog.show();
+
+                                            }
+                                        }
+
 
 
                                     }
@@ -137,5 +146,28 @@ public class QRCheckIn_fragment extends Fragment {
             //Navigation.findNavController(requireView()).navigate(R.id.action_QRCheckIn_fragment_to_attendeeSelection_fragment);
         }
     });
+
+    void showCustomDialog(){
+        final Dialog dialog = new Dialog(thisContext);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(true);
+        dialog.setContentView(R.layout.fragment_name_dialog_box);
+
+        final EditText nameEt = dialog.findViewById(R.id.dialogName);
+        final EditText emailAddressEt = dialog.findViewById(R.id.dialogEmailAddress);
+        final EditText phoneNumberEt = dialog.findViewById(R.id.dialogPhoneNumber);
+        final EditText addressEt = dialog.findViewById(R.id.dialogAddress);
+        Button cancel = dialog.findViewById(R.id.cancelButton);
+        Button next = dialog.findViewById(R.id.nextButton);
+        next.setOnClickListener((v -> {
+            String name = nameEt.getText().toString();
+            String emailAddress = emailAddressEt.getText().toString();
+            String phoneNumber = phoneNumberEt.getText().toString();
+            dialog.dismiss();
+        }));
+
+        dialog.show();
+
+    }
 }
 
