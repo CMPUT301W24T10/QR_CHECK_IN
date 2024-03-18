bc77ab819ccbfd4941a8e8b19d800fb53f6b14cc
norahkallely13 <norahkallely@gmail.com>
2024-03-17 19:46:48 -0600

    Updating Announcement Fragment in US 02.04.01 to allow attendee to view all list of events and their event details

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -52,6 +52,10 @@ dependencies {
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
 
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+
+
 
     implementation ("com.google.zxing:core:3.3.0")
     implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
